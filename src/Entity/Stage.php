2879dbfe4c51b0c89364131db74c1a7f75f4c505
Guillaume Peyrelongue <gpeyrelongue@iutbayonne.univ-pau.fr>
2022-01-08 14:45:18 +0100

    relation terminer

--- src/Entity/Stage.php
+++ src/Entity/Stage.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\StageRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -31,6 +33,22 @@ class Stage
      * @ORM\Column(type="string", length=50)
      */
     private $contact;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Entreprise::class, inversedBy="stages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $codeEntreprise;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Formation::class, inversedBy="stages")
+     */
+    private $codeFormation;
+
+    public function __construct()
+    {
+        $this->codeFormation = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -69,6 +87,42 @@ class Stage
     public function setContact(string $contact): self
     {
         $this->contact = $contact;
+
+        return $this;
+    }
+
+    public function getCodeEntreprise(): ?Entreprise
+    {
+        return $this->codeEntreprise;
+    }
+
+    public function setCodeEntreprise(?Entreprise $codeEntreprise): self
+    {
+        $this->codeEntreprise = $codeEntreprise;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Formation[]
+     */
+    public function getCodeFormation(): Collection
+    {
+        return $this->codeFormation;
+    }
+
+    public function addCodeFormation(Formation $codeFormation): self
+    {
+        if (!$this->codeFormation->contains($codeFormation)) {
+            $this->codeFormation[] = $codeFormation;
+        }
+
+        return $this;
+    }
+
+    public function removeCodeFormation(Formation $codeFormation): self
+    {
+        $this->codeFormation->removeElement($codeFormation);
 
         return $this;
     }
