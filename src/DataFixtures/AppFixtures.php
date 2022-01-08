7b4d90c1b50ade812e1b07085ff7476e8c26cd72
Guillaume Peyrelongue <gpeyrelongue@iutbayonne.univ-pau.fr>
2022-01-08 18:06:45 +0100

    ajout jeu de test

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,107 +19,97 @@ class AppFixtures extends Fixture
 
 
         // Création des entreprises///////////////////
-        $tabEntreprise[5];
+        $tabEntreprise=array();
         
-        for ($i=1; $i <= 5;$i++){
+        for ($i=1; $i <= 5;$i++)
+        {
             $nomCompanie = $faker->company;
-        $Entreprise = new Entreprise();
-        $Entreprise->setNom($nomCompanie);
-        $Entreprise->setAdresse($faker->address);
-        $Entreprise->setActivite($faker->realText($maxNbChars =50, $indexSize = 2));
-        $Entreprise->setUrlSite("http://$nomCompanie.com");
-        //"Création de jeux video""Volinir""https://Volinir.com""6 rue des ballons, Bordeaux"
-        $manager->persist($Entreprise);
+            $entreprise = new Entreprise();
+            $entreprise->setNom($nomCompanie);
+            $entreprise->setAdresse($faker->address);
+            $entreprise->setActivite($faker->realText($maxNbChars =50, $indexSize = 2));
+            $entreprise->setUrlSite("http://$nomCompanie.com");
+            //"Création de jeux video""Volinir""https://Volinir.com""6 rue des ballons, Bordeaux"
+            $manager->persist($entreprise);
 
-        $tabEntreprise[$i]=$Entreprise;
+            $tabEntreprise[$i]=$entreprise;
         }
         //////////////////////////////////////////////
 
+        
         //creation des formation
 
-        $tabFormation =array(
-            "DUT Informatique" => "Diplôme Universitaire et Thechnologique Informatique",
+        $tabNomFormations =array(
+            "DUT Informatique" => "Diplôme Universitaire Thechnologique Informatique",
             "LAES" => "Licence Administration économique et sociale",
             "LCAA" =>   "Licence Cinéma - Audiovisuel - Arts",
-            "DUT GIM" =>    "Diplôme Universitaire et Thechnologique Génie Industriel et Maintenance",
+            "DUT GIM" =>    "Génie Industriel et Maintenance",
 
         );
 
+        $tabFormations=array();
 
-        foreach ($tabFormation as $nomLong => $nomCourt){
-        $formation= new Formation();
-        $formation->setNom($nomLong);
-        $formation->setNomCourt($nomCourt);
-        $manager->persist($formation);
-
-        }
-        
-
-
-
-        foreach($tabEntreprise as $uneEntreprise)
+        foreach ($tabNomFormations as $nomCourt => $nomLong)
         {
-            $nbAleatoire=$faker->numberBetween($min = 1 , $max = 5);  // nombre de stage alétoire
-            for ($i = 0; $i<$nbAleatoire; $i++){
-                $stage = new Stage();
-                $stage->setTitre($faker->jobTitle );
-                $stage->setMissions($faker->realText($maxNbChars =200, $indexSize = 2));
-                $nom=$uneEntreprise->getNom();
-                $stage->setContact("$nom@gmail.com");
+            $formation= new Formation();
+            $formation->setNom($nomLong);
+            $formation->setNomCourt($nomCourt);
+            
+            $tabFormations[]=$formation;
                 
-            }
-
-
-
-
+            $manager->persist($formation);
 
         }
+        
+        $nbformation=count($tabFormations);
+            
+
+            foreach($tabEntreprise as $uneEntreprise)
+            {
+                $nbAleatoire=$faker->numberBetween($min = 1 , $max = 5);  // nombre de stage alétoire
+                for ($i = 0; $i<$nbAleatoire; $i++){
+                    $stage = new Stage();
+                    $stage->setTitre("BOB" );
+                    $stage->setMissions($faker->realText($maxNbChars =200, $indexSize = 2));
+                    $nom=$uneEntreprise->getNom();
+                    $stage->setContact("$nom@gmail.com");
+
+                    //nombre aléatoire 
+                    $nbAleaFormation=$faker->numberBetween($min = 0 , $max = $nbformation-1);
+                    $formationT = $tabFormations[$nbAleaFormation];
+                    //LES LIAISONS//
+
+                    //ajout d'une formation dans le stage
+                    $stage->addCodeFormation($formationT); //add car le stage peut etre lier a plusieurs entreprises 
+                    
+                    //ajout d'une entreprise pour le stage
+                    $stage->setCodeEntreprise($uneEntreprise);//set car le stage ne peut avoir qu'une entreprise
+                    
+                    //ajout du stage pour l'entreprise
+                    $uneEntreprise->addStage($stage);
+
+
+                    $manager->persist($stage);
+                    $manager->persist($uneEntreprise);
+                    
+
+                }
 
 
 
 
+            }
+            
+
+           
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        $manager->flush();
+            $manager->flush();
+      
 
 
         
-
+       
 
     }
 }
